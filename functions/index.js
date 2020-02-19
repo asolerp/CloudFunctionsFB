0d77ac68f0dd83030ef7df9c836f92d0f0859be8
Alberto <asoler@topdoctors.es>
2020-02-19 18:02:12 +0100

    Created cloud function to send notification

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const axios = require('axios')
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -30,8 +31,103 @@ exports.updateParticipation = functions.firestore
           assistance += 1
         }
       })
-      const promise = db.collection('matches').doc(context.matchID).update({
+      return db.collection('matches').doc(context.params.matchID).update({
         assistance: assistance
-      }).then(() => console.log(`Participación actualizada`))
-      return Promise.all(promise)
+      })
+      .then(() => console.log(`Participación actualizada`))
+      .catch(e => console.log(e))
     });
+
+
+    //send the push notification 
+exports.sendPushNotification = functions.firestore
+    .document('matches/{matchID}').onUpdate((change, context) => {
+
+    const newValue = change.after.data();
+    let players = []
+    let messages = []
+  //return the main promise 
+
+  const config = {
+    headers: { 
+      'Content-Type': 'application/json', 
+      'accept-encoding': 'gzip, deflate',   
+      'host': 'exp.host'  
+    }
+  }
+
+  players.push(axios({
+      method: 'post',
+      url: 'https://exp.host/--/api/v2/push/send',
+      headers: { 
+        'Content-Type': 'application/json', 
+        'accept-encoding': 'gzip, deflate',   
+        'host': 'exp.host'  
+      }, 
+      data: {
+        "to": "ExponentPushToken[rzZvAvIkzHFXoOnqWwLChF]",
+        "title": "Hola!!!",
+        "tag": "main-tag",
+        "body": "Te veo desde Cloud!!!!!!!"
+      }      
+    })
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err))
+  )
+
+  return Promise.all(players)
+  
+  // axios.post('https://api.ipify.org?format=json')
+
+  // players.push(
+  //   fetch('https://exp.host/--/api/v2/push/send', {       
+  //        method: 'POST', 
+  //        headers: {
+  //              Accept: 'application/json',  
+  //             'Content-Type': 'application/json', 
+  //             'accept-encoding': 'gzip, deflate',   
+  //             'host': 'exp.host'      
+  //         }, 
+  //       body: JSON.stringify({                 
+  //             to: 'ExponentPushToken[rzZvAvIkzHFXoOnqWwLChF]',                        
+  //             title: 'New Notification',                  
+  //             body: 'The notification worked!',             
+  //             priority: "high",            
+  //             sound:"default",              
+  //             channelId:"default",   
+  //           }),        
+  //         })
+  // )
+
+  // Object.keys(newValue.participation).forEach(async uid => {
+  //   players.push(db.collection('users')
+  //   .doc(uid)
+  //   .get()
+  //   .then(doc => {
+  //     let expoToken = doc.data().expoToken;
+  //     if (expoToken) {
+  //       messages.push(fetch('https://exp.host/--/api/v2/push/send', {       
+  //        method: 'POST', 
+  //        headers: {
+  //              Accept: 'application/json',  
+  //             'Content-Type': 'application/json', 
+  //             'accept-encoding': 'gzip, deflate',   
+  //             'host': 'exp.host'      
+  //         }, 
+  //       body: JSON.stringify({                 
+  //             to: expoToken,                        
+  //             title: 'New Notification',                  
+  //             body: 'The notification worked!',             
+  //             priority: "high",            
+  //             sound:"default",              
+  //             channelId:"default",   
+  //           }),        
+  //         })
+  //       );
+  //     }
+  //     return Promise.all(messages)
+  //   })
+  //   )
+  // })
+
+});
